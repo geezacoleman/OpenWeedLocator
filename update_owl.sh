bd619ca3c13581939adcfe30c43f91de617b56d1
Guy Coleman <51358498+geezacoleman@users.noreply.github.com>
2023-05-24 19:21:30 +0200

    Create update_owl.sh
    add in a file to automate the updating process

---
+++ update_owl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ~
+
+# Rename the old 'owl' folder to 'owl-old'
+mv owl owl-old     
+
+# Download the new software from GitHub
+git clone https://github.com/geezacoleman/OpenWeedLocator owl     
+cd ~/owl
+
+# update the system
+echo "[INFO] Upgrading Raspberry Pi system...this may take some time. You will be asked to confirm at some steps."
+sudo apt-get update
+sudo apt-get upgrade
+
+# Installing the requirements
+echo "[INFO] Upgrading OWL requirements."
+workon 'owl'
+pip install -r requirements.txt
+
+# Changing permissions to make files executable
+chmod a+x owl.py
+chmod a+x owl_boot.sh
+
+echo "[COMPLETE] OWL update has executed successfully."
