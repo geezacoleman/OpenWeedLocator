f176fac497c538d111045c44ce7e24d66112a004
geezacoleman <guy@openag.tech>
2025-07-09 19:12:24 +0200

    update config_manager.py and config files

--- utils/config_manager.py
+++ utils/config_manager.py
@@ -12,8 +12,12 @@ class ConfigValidator:
 
     REQUIRED_CONFIG = {
         'System': {
-            'required_keys': {'algorithm', 'relay_num', 'actuation_duration', 'delay', 'dashboard_enable'},
+            'required_keys': {'algorithm', 'relay_num', 'actuation_duration', 'delay'},
             'optional_keys': {'input_file_or_directory'}
+        },
+        'Dashboard': {
+            'required_keys': {'dashboard_enable', 'dashboard_port', 'gps_source', 'gps_port'},
+            'optional_keys': {'gps_baudrate'}
         },
         'Controller': {
             # Base requirements for all controller types
